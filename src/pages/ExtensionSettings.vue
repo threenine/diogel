24a37923bcd8c8f8aa018dbdbd98a8fc7dc4ddb3
Gary Woodfine <annex-storage-slip@duck.com>
2025-12-12 20:15:54 +0000

    Style settings page: add layout, theming updates, and form container

--- src/pages/ExtensionSettings.vue
+++ src/pages/ExtensionSettings.vue
@@ -3,24 +3,27 @@ import ThemeSwitch from 'components/ThemeSwitch.vue';
 </script>
 
 <template>
-  <q-page class="row items-center justify-evenly">
-    <div class="q-pa-lg-lg">
-      <q-list>
-        <q-item v-ripple tag="label">
-          <q-item-section>
-            <q-item-label>Enable Dark theme</q-item-label>
-          </q-item-section>
-          <q-item-section avatar>
-            <theme-switch />
-          </q-item-section>
-        </q-item>
-      </q-list>
+  <q-page>
+    <div class="settings-container">
+      <div class="settings-form shadow-0 rounded-borders">
+        <div class="q-pa-md">
+          <q-list>
+            <q-item v-ripple tag="label">
+              <q-item-section>
+                <q-item-label>Enable Dark theme</q-item-label>
+                <q-item-label caption lines="2"
+                  >Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit
+                  elit.</q-item-label
+                >
+              </q-item-section>
+              <q-item-section side top>
+                <theme-switch />
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </div>
+      </div>
     </div>
   </q-page>
 </template>
-
-<style lang="sass">
-.settings-row-equal-width
-  .row > div
-    background: rgba(#999,.15)
-</style>
+<style scoped></style>
