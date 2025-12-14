91c3b29e9c7f00065d06bd8551b05234bcb9fc96
Gary Woodfine <annex-storage-slip@duck.com>
2025-12-14 20:09:53 +0000

    Add `q-btn` and `q-input` to CreateAccount page with basic bindings

--- src/pages/CreateAccount.vue
+++ src/pages/CreateAccount.vue
@@ -1,8 +1,16 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const text = ref('test')
+</script>
 
 <template>
   <q-page>
     <p>Create Account</p>
+    <q-btn label="Create Account" />
+    <div class="settings-container">
+      <q-input v-model="text" borderless label="Borderless" />
+    </div>
   </q-page>
 </template>
 
