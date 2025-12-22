21e4a1feedb247ca2b29dbe80550d15c87a61317
Gary Woodfine <lnb0l9dc@duck.com>
2025-12-22 11:10:31 +0000

    Rename project to "Diogel" and update logo with new styling adjustments.

--- src/components/PopupHeader.vue
+++ src/components/PopupHeader.vue
@@ -1,8 +1,18 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+
+</script>
 
 <template>
+  <div class="q-pa-md q-gutter-sm">
   <q-header elevated>
-    <q-img alt="Diogel" sizes="4xl" src="src/assets/diogel.png" title="Diogel" /></q-header>
+    <q-img alt="Diogel"
+           src="https://res.cloudinary.com/threenine-co-uk/image/upload/v1690313878/diogel_artpfy.png"
+           title="Diogel"
+           style="height: 40px; max-width: 40px"
+           class="q-ma-md q-pa-xs"
+
+    /></q-header>
+  </div>
 </template>
 
 <style scoped></style>
