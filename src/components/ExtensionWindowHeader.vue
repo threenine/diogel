ba47579b1a25db8012966dcfed1f70b54ea5c02a
Gary Woodfine <gary.woodfine@threenine.co.uk>
2026-04-05 07:48:10 +0100

    feat: conditionally render AccountDropdown based on vaultStore unlock state

--- src/components/ExtensionWindowHeader.vue
+++ src/components/ExtensionWindowHeader.vue
@@ -1,17 +1,21 @@
 <script lang="ts" setup>
 import AccountDropdown from 'components/AccountDropdown/Index.vue';
 import MainNavigation from 'components/MainNavigation.vue';
+import useVaultStore from 'src/stores/vault-store';
+
+const vaultStore = useVaultStore();
+
 </script>
 
 <template>
-  <q-header >
+  <q-header>
     <q-toolbar>
       <div class="q-pa-lg-lg">
         <main-navigation />
         <q-img src="/images/diogel.svg" style="width: 35px; height: 35px" />
       </div>
       <q-space />
-      <div class="q-pa-lg-lg"><account-dropdown /></div>
+      <div v-if="vaultStore.isUnlocked" class="q-pa-lg-lg"><account-dropdown /></div>
     </q-toolbar>
   </q-header>
 </template>
